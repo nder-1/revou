00bd2dbabfeafb8562f2de9ee47903e1980d3ad5
Sander <sandervdb23@gmail.com>
2024-08-12 20:48:28 +0700

    tes js pertama

---
+++ index.js
@@ -0,0 +1,8 @@
+function peringatan() {
+        let pertama = document.getElementById("angka-pertama").value;
+        let kedua = document.getElementById("angka-kedua").value;
+
+        let hasil = parseInt(pertama) + parseInt(kedua);
+
+        alert(hasil)
+        }
